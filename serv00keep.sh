2a0ddc2a2cead82bd1cc842267480311a0651ef3
kwzzz03 <kwzz03@gmail.com>
2025-02-16 17:15:59 +0800

    Update serv00keep.sh

--- serv00keep.sh
+++ serv00keep.sh
@@ -397,7 +397,7 @@ hy3p=$(sed -n '3p' hy2ip.txt)
     ],
     "transport": {
       "type": "ws",
-      "path": "$UUID-vm",
+      "path": "/",
       "early_data_header_name": "Sec-WebSocket-Protocol"
       }
     }
@@ -587,11 +587,11 @@ NAME="$ISP-$(get_name)"
 rm -rf jh.txt
 vl_link="vless://$UUID@$IP:$vless_port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=$reym&fp=chrome&pbk=$public_key&type=tcp&headerType=none#$NAME-reality"
 echo "$vl_link" >> jh.txt
-vmws_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws\", \"add\": \"$IP\", \"port\": \"$vmess_port\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\", \"path\": \"/$UUID-vm?ed=2048\", \"tls\": \"\", \"sni\": \"\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)"
+vmws_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws\", \"add\": \"$IP\", \"port\": \"$vmess_port\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\", \"path\": \"/\", \"tls\": \"\", \"sni\": \"\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)"
 echo "$vmws_link" >> jh.txt
-vmatls_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws-tls-argo\", \"add\": \"icook.hk\", \"port\": \"8443\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/$UUID-vm?ed=2048\", \"tls\": \"tls\", \"sni\": \"$argodomain\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)"
+vmatls_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws-tls-argo\", \"add\": \"icook.hk\", \"port\": \"8443\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/\", \"tls\": \"tls\", \"sni\": \"$argodomain\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)"
 echo "$vmatls_link" >> jh.txt
-vma_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws-argo\", \"add\": \"icook.hk\", \"port\": \"8880\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/$UUID-vm?ed=2048\", \"tls\": \"\"}" | base64 -w0)"
+vma_link="vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmess-ws-argo\", \"add\": \"icook.hk\", \"port\": \"8880\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/\", \"tls\": \"\"}" | base64 -w0)"
 echo "$vma_link" >> jh.txt
 hy2_link="hysteria2://$UUID@$IP:$hy2_port?sni=www.bing.com&alpn=h3&insecure=1#$NAME-hy2"
 echo "$hy2_link" >> jh.txt
@@ -747,7 +747,7 @@ cat > sing_box.json <<EOF
                         "www.bing.com"
                     ]
                 },
-                "path": "/$UUID-vm",
+                "path": "/",
                 "type": "ws"
             },
             "type": "vmess",
@@ -790,7 +790,7 @@ cat > sing_box.json <<EOF
                         "$argodomain"
                     ]
                 },
-                "path": "/$UUID-vm",
+                "path": "/",
                 "type": "ws"
             },
             "type": "vmess",
@@ -817,7 +817,7 @@ cat > sing_box.json <<EOF
                         "$argodomain"
                     ]
                 },
-                "path": "/$UUID-vm",
+                "path": "/",
                 "type": "ws"
             },
             "type": "vmess",
@@ -978,7 +978,7 @@ proxies:
   network: ws
   servername: www.bing.com                    
   ws-opts:
-    path: "/$UUID-vm"                             
+    path: "/"                             
     headers:
       Host: www.bing.com                     
 
@@ -1005,7 +1005,7 @@ proxies:
   network: ws
   servername: $argodomain                    
   ws-opts:
-    path: "/$UUID-vm"                             
+    path: "/"                             
     headers:
       Host: $argodomain
 
@@ -1021,7 +1021,7 @@ proxies:
   network: ws
   servername: $argodomain                   
   ws-opts:
-    path: "/$UUID-vm"                             
+    path: "/"                             
     headers:
       Host: $argodomain 
 
